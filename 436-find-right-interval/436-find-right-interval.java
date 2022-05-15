088049fcb1345c4d26e0ee757d01546d6dd9ed8c
Udipta18 <105000386+Udipta18@users.noreply.github.com>
2022-05-15 19:42:22 +0530

    Time: 596 ms (9.32%), Space: 55.8 MB (38.85%) - LeetHub

---
+++ 436-find-right-interval/436-find-right-interval.java
@@ -0,0 +1,28 @@
+class Solution {
+
+   public int[] findRightInterval(int[][] intervals) {
+            int n = intervals.length;
+            if (n == 1)
+                return new int[]{-1};
+
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = getIndex(intervals, intervals[i][1]);
+            }
+
+            return result;
+        }
+
+        private int getIndex(int[][] intervals, int key) {
+            for (int i = 0; i < intervals.length; i++) {
+                if (key == intervals[i][0])
+                    return i;
+            }
+            for (int i = 0; i < intervals.length; i++) {
+                if (key < intervals[i][0])
+                    return i;
+            }
+            return -1;
+        }
+    }
+
