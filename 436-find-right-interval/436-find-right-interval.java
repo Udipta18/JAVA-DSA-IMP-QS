8f56938adc99131e3904ba00eb83fed0596fc04d
Udipta18 <105000386+Udipta18@users.noreply.github.com>
2022-05-15 19:45:26 +0530

    Time: 8 ms (96.69%), Space: 46.1 MB (94.98%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.java
+++ 436-find-right-interval/436-find-right-interval.java
@@ -1,28 +1,41 @@
+
 class Solution {
 
-   public int[] findRightInterval(int[][] intervals) {
-            int n = intervals.length;
-            if (n == 1)
-                return new int[]{-1};
+  public int[] findRightInterval(int[][] intervals) {
+    int n = intervals.length;
+    int[] res = new int[n];
+	//because the start value is unique, save s and index in map
+    HashMap<Integer, Integer> map = new HashMap<>();
+	//save the start
+    int[] s = new int[n];
+    for (int i = 0; i < n; i++) {
+        s[i] = intervals[i][0];
+        map.put(s[i], i);
+    }
+	//sort the start arr
+    Arrays.sort(s);
+    for (int i = 0; i < n; i++) {
+        res[i] = binarySearch(s, map, intervals[i][1]);
+    }
+    return res;
+}
 
-            int[] result = new int[n];
-            for (int i = 0; i < n; i++) {
-                result[i] = getIndex(intervals, intervals[i][1]);
-            }
-
-            return result;
-        }
-
-        private int getIndex(int[][] intervals, int key) {
-            for (int i = 0; i < intervals.length; i++) {
-                if (key == intervals[i][0])
-                    return i;
-            }
-            for (int i = 0; i < intervals.length; i++) {
-                if (key < intervals[i][0])
-                    return i;
-            }
-            return -1;
+//binarysearch to get the first one who largger than v, and return the index in map
+int binarySearch(int[] s, HashMap<Integer, Integer> map, int v) {
+    if (v <= s[0]) {
+        return map.get(s[0]);
+    } else if (v > s[s.length - 1]) {
+        return -1;
+    }
+    int l = 0, r = s.length;
+    while (l < r) {
+        int mid = (l + r) >>> 1;
+        if (s[mid] >= v) {
+            r = mid;
+        } else {
+            l = mid + 1;
         }
     }
-
+    return map.get(s[l]);
+}
+    }
