1d4a4aa0065537259b3f78ca21d759caa2f2593a
Udipta18 <105000386+Udipta18@users.noreply.github.com>
2022-05-23 14:02:44 +0530

    Time: 9 ms (29.42%), Space: 41.1 MB (62.93%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int row=0;
+        while(n>0){
+ 
+            
+            row++;
+            n-=row;
+        
+        
+        }
+        
+        return n==0?row:row-1;
+    }
+}
