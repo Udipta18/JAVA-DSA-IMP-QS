d1770ffb7d47f9a46915938912e858fc724d9327
Udipta18 <105000386+Udipta18@users.noreply.github.com>
2022-06-12 18:40:50 +0530

    Time: 7 ms (47.36%), Space: 40.6 MB (61.74%) - LeetHub

---
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+      
+        if (c < 0) {
+            return false;
+        }
+        long left = 0, right = (int)Math.sqrt(c);
+        while (left <= right) {
+            long cur = left * left + right * right;
+            if (cur < c) {
+                left++;
+            } else if (cur > c) {
+                right--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
